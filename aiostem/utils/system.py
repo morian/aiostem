6347013b35310fdbc9599b0f4fec4c7bfcab4e2c
Vizonex <VizonexBusiness@gmail.com>
2025-09-03 12:40:40 -0500

    conform to ruff's suggestions

--- aiostem/utils/system.py
+++ aiostem/utils/system.py
@@ -6,45 +6,48 @@ import asyncio
 import enum
 import os
 import platform
-import threading
-from typing import TYPE_CHECKING, Any, overload
 from functools import lru_cache
-from ..types import _T
+from typing import TYPE_CHECKING, Any, overload
 
 if TYPE_CHECKING:
     from pathlib import Path
+
+    from ..types import _T
 
 
 # Sentinal Value for call function
 _UNDEFINED = enum.Enum('_UNDEFINED', 'UNDEFINED')
 UNDEFINED = _UNDEFINED.UNDEFINED
 
+
 class CallError(OSError):
     """
-    Error response when making a system call. This is an **OSError** subclass
+    Error response when making a system call.
+
+    This is an **OSError** subclass
     with additional information about the process. Depending on the nature of the
     error not all of these attributes will be available.
-
-    :var str msg: exception string
-    :var str command: command that was ran
-    :var int exit_status: exit code of the process
-    :var float runtime: time the command took to run
-    :var str stdout: stdout of the process
-    :var str stderr: stderr of the process
     """
 
-    __slots__ = ('msg', 'command', 'exit_status', 'runtime', 'stdout', 'stderr')
+    __slots__ = ('command', 'exit_status', 'runtime', 'stderr', 'stdout')
 
     def __init__(
         self,
-        msg: str,
         command: str,
         exit_status: int,
         runtime: float,
         stdout: str,
         stderr: str,
     ):
-        self.msg = msg
+        """
+        Return CallError.
+
+        :var str command: command that was ran
+        :var int exit_status: exit code of the process
+        :var float runtime: time the command took to run
+        :var str stdout: stdout of the process
+        :var str stderr: stderr of the process.
+        """
         self.command = command
         self.exit_status = exit_status
         self.runtime = runtime
@@ -52,57 +55,59 @@ class CallError(OSError):
         self.stderr = stderr
 
     def __str__(self):
-        return self.msg
+        """
+        Return CallError's message as a string.
+
+        :returns str: returns msg as a string
+        """
+        return f'{self.command} returned exit status {self.exit_status}'
 
 
 def is_windows():
     """
-    Checks if we are running on Windows.
+    Return weather or not we are running Windows.
 
     :returns: **bool** to indicate if we're on Windows
     """
-
     return platform.system() == 'Windows'
 
 
 def is_mac():
     """
-    Checks if we are running on Mac OSX.
+    Return weather or not we are running Mac OSX.
 
     :returns: **bool** to indicate if we're on a Mac
     """
-
     return platform.system() == 'Darwin'
 
 
 def is_gentoo():
     """
-    Checks if we're running on Gentoo.
+    Return weather or not we are running Gentoo.
 
     :returns: **bool** to indicate if we're on Gentoo
     """
-
     return os.path.exists('/etc/gentoo-release')
 
 
 def is_slackware():
     """
-    Checks if we are running on a Slackware system.
+    Return n weather or not we are running on a Slackware system.
 
     :returns: **bool** to indicate if we're on a Slackware system
     """
-
     return os.path.exists('/etc/slackware-version')
 
 
 def is_bsd():
     """
-    Checks if we are within the BSD family of operating systems. This currently
-    recognizes Macs, FreeBSD, and OpenBSD but may be expanded later.
+    Return if we are within the BSD family of operating systems. T.
+
+    This currently recognizes Macs, FreeBSD, and OpenBSD but may
+    be expanded later.
 
     :returns: **bool** to indicate if we're on a BSD OS
     """
-
     return platform.system() in ('Darwin', 'FreeBSD', 'OpenBSD', 'NetBSD')
 
 
@@ -113,7 +118,7 @@ async def call(
     default: _T,
     ignore_exit_status: bool = False,
     timeout: float | None = None,
-    cwd: bytes | str | 'Path' | None = None,
+    cwd: bytes | str | Path | None = None,
     env: dict[str, Any] | None = None,
 ) -> _T | list[str]: ...
 
@@ -124,7 +129,7 @@ async def call(
     default: _UNDEFINED,
     ignore_exit_status: bool = False,
     timeout: float | None = None,
-    cwd: bytes | str | 'Path' | None = None,
+    cwd: bytes | str | Path | None = None,
     env: dict[str, Any] | None = None,
 ) -> list[str]: ...
 
@@ -134,12 +139,13 @@ async def call(
     default: _UNDEFINED | _T = _UNDEFINED,
     ignore_exit_status: bool = False,
     timeout: float | None = None,
-    cwd: bytes | str | 'Path' | None = None,
+    cwd: bytes | str | Path | None = None,
     env: dict[str, Any] | None = None,
 ) -> _T | list[str]:
     """
-    Issues a command in a subprocess, blocking until completion and returning the
-    results. This is not actually ran in a shell so pipes and other shell syntax
+    Issues a command in a subprocess, blocking until completion and returning the results.
+
+    This is not actually ran in a shell so pipes and other shell syntax
     are not permitted.
 
     :param str,list command: command to be issued
@@ -158,10 +164,7 @@ async def call(
     """
     loop = asyncio.get_event_loop()
 
-    if isinstance(command, str):
-        command_list = command.split(' ')
-    else:
-        command_list = list(map(str, command))
+    command_list = command.split(' ') if isinstance(command, str) else list(map(str, command))
 
     exit_status, runtime, stdout, stderr = None, None, None, None
     start_time = loop.time()
@@ -194,41 +197,39 @@ async def call(
         exit_status = await process.wait()
 
         if not ignore_exit_status and exit_status != 0:
-            raise OSError('%s returned exit status %i' % (command, exit_status))
+            if default != UNDEFINED:
+                return default
+            raise CallError(
 
-        if stdout:
-            return stdout.decode('utf-8', 'replace').splitlines()
-        else:
-            return []
+                ' '.join(command_list),
+                exit_status,
+                runtime,
+                stdout,
+                stderr
+            )
+
+
+        return stdout.decode('utf-8', 'replace').splitlines() if stdout else []
 
     # There really isn't a point to using CallTimeoutError since
     # it seems that was just a python 2 backport.
     except TimeoutError:
         if default != UNDEFINED:
             return default
-        else:
-            raise
-
-    except OSError as exc:
-        if default != UNDEFINED:
-            return default
-        else:
-            raise CallError(
-                str(exc), ' '.join(command_list), exit_status, runtime, stdout, stderr
-            )
+        raise
 
 
 
 CMD_AVAILABLE_CACHE = {}
 
-def _is_available(command:str):
 
+def _is_available(command: str):
     if ' ' in command:
-        command = command[:command.find(' ')]
+        command = command[: command.find(' ')]
 
     if command == 'ulimit':
         return True  # we can't actually look it up, so hope the shell really provides it...
-    elif 'PATH' not in os.environ:
+    if 'PATH' not in os.environ:
         return False  # lacking a path will cause find_executable() to internally fail
 
     cmd_exists = False
@@ -245,14 +246,17 @@ def _is_available(command:str):
 
     return cmd_exists
 
+
 lru_is_avalible = lru_cache(typed=False, maxsize=128)(_is_available)
 
 # XXX: _is_available is not entirely async so we need to thread it.
 
-async def is_avalible(command:str, cached:bool = True):
+
+async def is_avalible(command: str, cached: bool = True):
     """
-    Checks the current PATH to see if a command is available or not. If more
-    than one command is present (for instance "ls -a | grep foo") then this
+    Return if a command is available or not.
+
+    If more than one command is present (for instance "ls -a | grep foo") then this
     just checks the first.
 
     Note that shell (like cd and ulimit) aren't in the PATH so this lookup will
@@ -265,7 +269,6 @@ async def is_avalible(command:str, cached:bool = True):
     :returns: **True** if an executable we can use by that name exists in the
       PATH, **False** otherwise
     """
-    return await asyncio.to_thread(_is_available if not cached else lru_is_avalible, command=command)
-
-
-
+    return await asyncio.to_thread(
+        _is_available if not cached else lru_is_avalible, command=command
+    )
